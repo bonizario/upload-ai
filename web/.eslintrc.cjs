2742c9ae6f0ea6a4fcce14edb274cddf6c9f3fae
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-09-16 18:04:44 -0300

    chore: add eslint, prettier, and eslint-config-universe

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,57 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'universe/web',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        groups: ['builtin', 'external'],
+        pathGroups: [
+          {
+            pattern: '@app/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@assets/**',
+            group: 'internal',
+          },
+          {
+            pattern: '@view/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'parent', 'sibling', 'index', 'type', 'object'],
+      },
+    ],
+  },
+};
