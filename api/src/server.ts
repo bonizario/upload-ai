445a53572ce020ced37d12c38e8d9431c6279d89
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-09-17 16:12:32 -0300

    feat: create transcription endpoint

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import { fastify } from 'fastify';
 
+import { createTranscriptionRoute } from './routes/create-transcription';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
 
@@ -10,6 +12,8 @@ const app = fastify();
 app.register(getAllPromptsRoute);
 
 app.register(uploadVideoRoute);
+
+app.register(createTranscriptionRoute);
 
 app.listen({ port: PORT }).then(() => {
   console.log(`Server started at http://127.0.0.1:${PORT}`);
