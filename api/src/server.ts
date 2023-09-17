41645e14d12222e09834a6851761bb9c2a030c17
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2023-09-17 12:35:06 -0300

    feat: initial fastify/prisma setup

---
+++ api/src/server.ts
@@ -0,0 +1,13 @@
+import { fastify } from 'fastify';
+
+const PORT = 3333;
+
+const app = fastify();
+
+app.get('/', (request, response) => {
+  return response.send({ hello: 'world' });
+});
+
+app.listen({ port: PORT }).then(() => {
+  console.log(`Server started at http://localhost:${PORT}`)
+});
